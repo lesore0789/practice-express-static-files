7d220b5ab65b6195e7217d085856d70a131a2371
Leslie <lesore0789@gmail.com>
2023-03-24 13:52:38 -0700

    Complete assignment

--- server/app.js
+++ server/app.js
@@ -2,5 +2,17 @@ const express = require('express');
 const app = express();
 // Your code here
 
+// Part 1
+// app.use('/', express.static('assets'));
+
+// Part 2
+app.use('/assets', express.static('scripts'));
+
+// Part 3
+app.use('/stylesheets', express.static('assets/css'));
+
+// Bonus Practice
+app.use('/stickers', express.static('assets/images'));
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
